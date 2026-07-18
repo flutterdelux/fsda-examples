ce3bdbce044c99fb8e79a75ca1bdabccd3e9fed5
flutterdelux <flutterdelux@gmail.com>
2026-07-18 09:48:33 +0700

    inbox adjustment

--- FSDA-Base/modules/inbox/lib/src/features/inbox/inbox_feature.dart
+++ FSDA-Base/modules/inbox/lib/src/features/inbox/inbox_feature.dart
@@ -2,13 +2,11 @@
 export 'data/datasources/inbox_remote_data_source.dart';
 export 'data/datasources/inbox_remote_data_source_impl.dart';
 export 'data/repositories/inbox_repository_impl.dart';
-export 'domain/repositories/inbox_repository.dart';
 // domain
+export 'domain/repositories/inbox_repository.dart';
 export 'domain/usecases/inbox_mark_all_read_use_case.dart';
 // logic
 export 'logic/mark_all_read/inbox_mark_all_read_cubit.dart';
 export 'logic/mark_all_read/inbox_mark_all_read_state.dart';
 // ui
 export 'ui/mark_all_read/widgets/inbox_mark_all_read_popup_menu_item.dart';
-
-
